d0a5933c8d4c62a3b5d4e5d05760e631f1eee0a8
Dillon Dains <dilloncsbb@gmail.com>
2024-03-27 11:12:07 -0500

    updated some small things

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from . import db
-from datetime import datetime
+from datetime import datetime, timezone
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
-    completed = db.Column(db.Boolean, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     due_date = db.Column(db.String, nullable=False)
 
     
